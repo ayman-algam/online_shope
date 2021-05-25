fa2467f30c2aa73745b3bf60bc1f9369efa7db09
asa <aymanalgam1995@gmail.com>
2021-05-25 23:55:52 +0200

    app bar woman text Categories

--- lib/Screens/home_screen/components/body.dart
+++ lib/Screens/home_screen/components/body.dart
@@ -2,20 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
+import 'categories.dart';
 
 class Body extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding/2),
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
           child: Text(
             'Woman',
-            style: Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.bold),
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(fontWeight: FontWeight.bold),
           ),
-        )
+        ),
+        Categories(),
       ],
     );
   }
---
+++ lib/Screens/home_screen/components/categories.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+
+// ignore: use_key_in_widget_constructors
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand Bag", "jewellery", "FootWear", "Dresses"];
+
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: SizedBox(
+        height: 25,
+        child: ListView.builder(
+          itemCount: categories.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => buildCategory(index),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCategory(int index) {
+    return GestureDetector(
+      onDoubleTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              categories[index],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: selectedIndex == index ? kTextColor : kTextLightColor,
+              ),
+            ),
+            Container(
+              height: 2,
+              width: 30,
+              margin: const EdgeInsets.only(top: kDefaultPadding / 4),
+              color: selectedIndex == index ? Colors.black : Colors.transparent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
